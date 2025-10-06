d01e2ae1a5da8d927d668ceb8c6e35e8e9354326
Bruno barreto andrade pereira <bruno.pereira7@ba.estudante.senai.br>
2025-10-06 14:23:35 -0300

    Create teste codigo.py

---
+++ teste codigo.py
@@ -0,0 +1,23 @@
+import os 
+os.system("cls") 
+
+
+def lista_salario(desconto):
+    
+    return f"seu salario{salario,desconto}" 
+    
+
+salario=int(input("Digite seu salario:")) 
+valor_salario.append(salario)
+
+if salario >100:
+    desconto=0.30
+
+elif salario <100:
+    desconto=0.20
+
+else:
+    print("Digite seu salario jovem")
+
+resultado= lista_salario(salario)
+print(resultado)

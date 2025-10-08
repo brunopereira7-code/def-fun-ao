b84a5e82d082664d7daba8c24ff01c668324e4ee
Bruno barreto andrade pereira <bruno.pereira7@ba.estudante.senai.br>
2025-10-08 09:17:42 -0300

    Rev cod 6atividade.py

--- 6atividade.py
+++ 6atividade.py
@@ -22,13 +22,57 @@
 
 # lista_numeros(numeros,quantidades_impares,quantidades_pares)
 
- def saudar(nome):
-  """Esta função recebe um nome e retorna uma saudação."""
-  return f"Olá, {nome}!"
 
-# Chamando a função
-mensagem = saudar("Maria")
-print(mensagem) # Saída: Olá, Maria!
+import os 
+os.system("cls")
+numeros=[]
+quantidades_impares=0
+quantidades_pares=0
+
+def lista_numeros_corrigida(numeros, quantidades_impares, quantidade_pares):
+        
+    return {
+            "numeros": numeros,
+            "impares": quantidades_impares,
+            "pares": quantidade_pares
+        }
+    for i in range(4):
+        numero=int(input("digite seu numero"))
+        numeros.append(numero)
+        if numero % 2 ==0:
+            quantidades_pares+=1
+        else:
+            quantidades_impares+=1
+    # Chamando e armazenando o resultado
+    dados = lista_numeros_corrigida(numeros,quantidades_impares,quantidades_pares)
+    # def lista_numeros_corrigida(numeros, quantidade_pares, quantidades_impares):
+    #     # Agora a chamada: lista_numeros_corrigida(numeros, quantidade_pares, quantidades_impares)
+    #     # estaria correta.
+
+    # Agora a variável 'dados' armazena o dicionário, não 'None'.
+    print(f"\nLista de Números: {dados['numeros']}")
+    print(f"Quantidade de Pares: {dados['pares']}")
+    print(f"Quantidade de IMPares: {dados['impares']}")
+
+
+    # def saudar(nome):
+    #   """Esta função recebe um nome e retorna uma saudação."""
+    #   return f"Olá, {nome}!"
+
+    # # Chamando a função
+    # mensagem = saudar("Maria")
+    # print(mensagem) # Saída: Olá, Maria!
+
+
+
+#  def saudar(nome):
+#   """Esta função recebe um nome e retorna uma saudação."""
+#   return f"Olá, {nome}!"
+
+# # Chamando a função
+# mensagem = saudar("Maria")
+# print(mensagem) # Saída: Olá, Maria!
+
 
 
 
